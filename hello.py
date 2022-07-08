95d53c2b59b21f5b65cae74afe2e74f1fb7cb585
hatsawat2543 <87714897+hatsawat2543@users.noreply.github.com>
2022-07-08 11:38:11 +0700

    update

---
+++ hello.py
@@ -0,0 +1,28 @@
+print("Hello bro")
+"""
+Print ใช้แสดงข้อควาาม
+
+"""
+#string => ข้อความ ตัวอักษร
+#number => integer , float
+#boolean => true , false
+print(-1)
+print(0)
+print("1"+"9")
+print(3.99)
+print(True)
+print(False)
+print("Kong \n" + "Studio")
+
+# Data Type & Variable
+#ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+
+x = 20 #integer
+y = 3.99
+z = True
+#print("ผลลัพธ์ ="+str(x))
+#print(y)
+#print(z)
+print(Type(y))
+print(Type("kongsiam"))
+print(Type(z))
